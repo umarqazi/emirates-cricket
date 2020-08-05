b025f46b9b5ea290f5f91b5c235ba8f81126f68d
Fahad Shehzad <fahad.shehzad@codedistrict.com>
2020-08-05 13:32:47 +0500

    this line was causing an issue as at this point migrations didnot get executed causing error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Update;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $updates = Update::all();
+        //$updates = Update::all();
 
         View::share('marqueeUpdates', $updates);
     }
