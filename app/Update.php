74c92f4516b5660f49d147c4c2847854cf716e76
Fahad Shehzad <fahad.shehzad@codedistrict.com>
2020-08-05 13:26:59 +0500

    changed accessibility level of table attribute to protected,.

--- app/Update.php
+++ app/Update.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Update extends Model
 {
-    public $table = 'app_updates';
+    protected $table = 'app_updates';
     /**
      * The attributes that are mass assignable.
      *
