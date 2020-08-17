693fed5305a80b2cd0fc643fef14f5726c730967
Umar1857 <umar.farooq@gems.techverx.com>
2020-08-17 15:40:00 +0500

    - Dummy Files for Email Templates. - Created Routes.

---
+++ resources/views/email-templates/contact-reply.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+<h1>Contact Reply</h1>
+</body>
+</html>
---
+++ resources/views/email-templates/tournament-registration.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+<h1>Tournament Registration</h1>
+</body>
+</html>
