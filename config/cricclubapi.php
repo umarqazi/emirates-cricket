cd37df66a5c47a4d779b2f89f7ed255cfd85c05e
Umar1857 <umar.farooq@gems.techverx.com>
2020-09-16 20:13:08 +0500

    - Fixture Results on Fixtures Page.

---
+++ config/cricclubapi.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /**
+     *
+     * Below given are the URLS of CRIC-CLUB API's.
+     *
+     */
+
+    'get_clubId_and_league_list_url' => env('PROD_CRIC_CLUB_URL').'/league/cleague',
+    'get_club_info' => env('PROD_CRIC_CLUB_URL').'/clubs/info?clubId={clubId}',
+    'get_all_schedules_or_fixtures' => env('PROD_CRIC_CLUB_URL').'/schedule/1?clubId={clubId}',
+    'get_match_scorecard' => env('PROD_CRIC_CLUB_URL').'/liveScoreOverlay/getScorecard?clubId={clubId}&matchId={matchId}',
+    'get_match_score_overlay' => env('PROD_CRIC_CLUB_URL').'/liveScoreOverlay/liveScoreOverlayData?clubId={clubId}&matchId={matchId}&fixtureId={fixtureId}',
+
+];
