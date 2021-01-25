35019ebc370fe954a26c54ee074afce9a2fe5289
dawoodhashmi <dawood@gems.techverx.com>
2021-01-25 12:54:48 +0500

    -add 404 page in handler class -add new Api link in env file

---
+++ resources/views/frontend/404.blade.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<style>
+    .page-404 {
+        background-color: #8D0303;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    h2{
+        color: #fff;
+        margin: 0;
+        font-size: 75px;
+        font-family: sans-serif;
+     }
+    p{
+        margin: 0;
+        color: #fff;
+        font-size: 35px;
+        font-family: sans-serif;
+    }
+</style>
+</head>
+
+<body style="margin: 0; padding: 0">
+    <div class="page-404">
+        <h2>404</h2>
+        <p>Sorry, Page Not Found</p>
+    </div>
+
+
+</body>
+</html>
+
+
