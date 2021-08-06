d3ed996f4fc007b5b964793c1b0e1533851871ea
Umar1857 <umar.farooq@gems.techverx.com>
2021-08-06 16:07:57 +0500

    - Added social media links to email Templates.

--- resources/views/backend/partials/email-footer.blade.php
+++ resources/views/backend/partials/email-footer.blade.php
@@ -1,19 +1,13 @@
-<a href="#" class="email-icon">
+<a href="https://www.facebook.com/emiratescricket" class="email-icon">
     <img class="email-social-icon" src="{{ URL::asset('frontend/assets/images/email-facebook.png') }}" alt="">
 </a>
-<a href="#" class="email-icon">
+<a href="https://www.instagram.com/uaecricketofficial/" class="email-icon">
     <img class="email-social-icon" src="{{ URL::asset('frontend/assets/images/email-instagram.png') }}" alt="">
 </a>
-<a href="#" class="email-icon">
+<a href="https://twitter.com/EmiratesCricket" class="email-icon">
     <img class="email-social-icon" src="{{ URL::asset('frontend/assets/images/email-twitter.png') }}" alt="">
 </a>
-<a href="#" class="email-icon">
+<a href="https://www.youtube.com/channel/UCiU6arECb1-EkweiXnCUpPw" class="email-icon">
     <img class="email-social-icon" src="{{ URL::asset('frontend/assets/images/email-youtube.png') }}" alt="">
-</a>
-<a href="#" class="email-icon">
-    <img class="email-social-icon" src="{{ URL::asset('frontend/assets/images/email-linkedin.png') }}" alt="">
-</a>
-<a href="#" class="email-icon">
-    <img class="email-social-icon" src="{{ URL::asset('frontend/assets/images/email-pinterest.png') }}" alt="">
 </a>
 <p class="footer-text">{{env('APP_NAME')}}</p>
