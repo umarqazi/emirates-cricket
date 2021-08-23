2a82ceba2ca7088d67e5f6b696ab84acf5214079
Umar1857 <umar.farooq@gems.techverx.com>
2021-08-23 20:19:53 +0500

    - Added Favicon - Updated Logo

--- resources/views/backend/layout/master-backend.blade.php
+++ resources/views/backend/layout/master-backend.blade.php
@@ -12,8 +12,9 @@
 
     @yield('title')
 
-    <link rel="apple-touch-icon" href="{{URL::asset('backend/assets/images/favicon/apple-touch-icon-152x152.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{URL::asset('backend/assets/images/favicon/favicon-32x32.png')}}">
+    <link rel="icon" href="{{ URL::asset('backend/assets/images/favicon-img.png') }}" sizes="16x16">
+{{--    <link rel="apple-touch-icon" href="{{URL::asset('backend/assets/images/favicon/apple-touch-icon-152x152.png')}}">--}}
+{{--    <link rel="shortcut icon" type="image/x-icon" href="{{URL::asset('backend/assets/images/favicon/favicon-32x32.png')}}">--}}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- BEGIN: VENDOR CSS-->
     <link rel="stylesheet" type="text/css" href="{{URL::asset('backend/assets/css/vendors.min.css')}}">
@@ -114,7 +115,7 @@
 <!-- BEGIN: SideNav-->
 <aside class="sidenav-main nav-expanded nav-lock nav-collapsible sidenav-dark sidenav-active-rounded">
     <div class="brand-sidebar">
-        <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="{{route('dashboard')}}"><img src="{{URL::asset('backend/assets/images/logo/materialize-logo.png')}} " alt="{{env('APP_NAME')}}. logo"/><span class="logo-text hide-on-med-and-down">ECB</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
+        <h1 class="logo-wrapper"><a class="brand-logo darken-1" href="{{route('dashboard')}}"><img src="{{URL::asset('backend/assets/images/logo.png')}} " alt="{{env('APP_NAME')}} Logo"/><span class="logo-text hide-on-med-and-down">ECB</span></a><a class="navbar-toggler" href="#"><i class="material-icons">radio_button_checked</i></a></h1>
     </div>
     <ul class="sidenav sidenav-collapsible leftside-navigation collapsible sidenav-fixed menu-shadow" id="slide-out" data-menu="menu-navigation" data-collapsible="accordion">
         <li class="active bold"><a class="collapsible-header waves-effect waves-cyan " href="#"><i class="material-icons">settings_input_svideo</i><span class="menu-title" data-i18n="">Dashboard</span></a>
